e7cefb9ea8b8d0dd253eb7ef6445779e3bc17900
Manuel Pietschmann <mpi@dresden-elektronik.de>
2026-02-27 03:43:39 +0100

    Simplify SLIP protocol rx/tx, fix potential buffer overflow
    Also remove dependency on gcf.h.

--- protocol.c
+++ protocol.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 dresden elektronik ingenieurtechnik gmbh.
+ * Copyright (c) 2021-2026 dresden elektronik ingenieurtechnik gmbh.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -9,7 +9,6 @@
  */
 
 #include "protocol.h"
-#include "gcf.h"
 
 #define FR_END       (unsigned char)0xC0
 #define FR_ESC       (unsigned char)0xDB
@@ -17,153 +16,128 @@
 #define T_FR_ESC     (unsigned char)0xDD
 #define ASC_FLAG     0x01
 
-void PROT_SendFlagged(const unsigned char *data, unsigned len)
+static void protPutc(unsigned char c)
 {
-   unsigned char c = 0;
-   unsigned short i = 0;
-   unsigned short crc = 0;
-
-   /* put an end before the packet */
-   PROT_Putc(FR_END);
-
-   while (i < len)
-   {
-      c = data[i++];
-      crc += c;
-
-      switch (c)
-      {
-      case FR_ESC:
-         PROT_Putc(FR_ESC);
-         PROT_Putc(T_FR_ESC);
-         break;
-      case FR_END:
-         PROT_Putc(FR_ESC);
-         PROT_Putc(T_FR_END);
-         break;
-      default:
-         PROT_Putc(c);
-         break;
-      }
-   }
-
-   c = (~crc + 1) & 0xFF;
-   if (c == FR_ESC)
-   {
-      PROT_Putc(FR_ESC);
-      PROT_Putc(T_FR_ESC);
-   }
-   else if (c == FR_END)
-   {
-      PROT_Putc(FR_ESC);
-      PROT_Putc(T_FR_END);
-   }
-   else
-   {
-      PROT_Putc(c);
-   }
-
-   c = ( (~crc + 1) >> 8)   & 0xFF;
-   if (c == FR_ESC)
-   {
-      PROT_Putc(FR_ESC);
-      PROT_Putc(T_FR_ESC);
-   }
-   else if (c == FR_END)
-   {
-      PROT_Putc(FR_ESC);
-      PROT_Putc(T_FR_END);
-   }
-   else
-   {
-      PROT_Putc(c);
-   }
-
-   /* tie off the packet */
-   PROT_Putc(FR_END);
-
-   PROT_Flush();
+    switch (c)
+    {
+        case FR_ESC:
+            PROT_Putc(FR_ESC);
+            PROT_Putc(T_FR_ESC);
+            break;
+        case FR_END:
+            PROT_Putc(FR_ESC);
+            PROT_Putc(T_FR_END);
+            break;
+        default:
+            PROT_Putc(c);
+            break;
+    }
 }
 
-void PROT_ReceiveFlagged(PROT_RxState *rx, const unsigned char *data, unsigned len)
+void PROT_SendFlagged(const unsigned char *data, unsigned len)
 {
-    unsigned char c;
-    unsigned short pos = 0;
+    unsigned char c = 0;
+    unsigned short i = 0;
+    unsigned short crc = 0;
 
-   if (len == 0)
-   {
-       return;
-   }
+    /* put an end before the packet */
+    PROT_Putc(FR_END);
+
+    while (i < len)
+    {
+        c = data[i++];
+        crc += c;
+        protPutc(c);
+    }
+
+    crc = (~crc + 1);
+    protPutc(crc & 0xFF);
+    protPutc((crc >> 8) & 0xFF);
+
+    /* tie off the packet */
+    PROT_Putc(FR_END);
+    PROT_Flush();
+}
+
+int PROT_ReceiveFlagged(PROT_RxState *rx, const unsigned char *data, unsigned len)
+{
+    int err;
+    unsigned i;
+    unsigned char c;
+    unsigned short pos;
+    unsigned short crc;
+    unsigned short crc1;
+
+    pos = 0;
+    err = 0;
 
 nextTurn:
-   while(pos < len)
-   {
-      c = data[pos];
-      pos++;
+    for (; pos < len; )
+    {
+        pos++;
+        c = data[pos];
 
-      switch (c)
-      {
-      case FR_END:
-         if (rx->escaped)
-         {
-            /* invalid */
-            rx->bufpos = 0;
-            rx->crc = 0;
-         }
-         else
-         {
-            if (rx->bufpos >= 2)
+        if (c == FR_END)
+        {
+            if (rx->escaped)
             {
-               unsigned char crcvalid = 0;
-               /* Checksum bytes are added to the checksum rx->crc - substract them here */
-               rx->crc -= rx->buf[rx->bufpos-1];
-               rx->crc -= rx->buf[rx->bufpos-2];
-               /* TODO clean this messy condition up */
-               if ((((~(rx->crc)+1 ) & 0xFF) == rx->buf[rx->bufpos - 2]) &&
-                  ((((~(rx->crc)+1 ) >> 8) & 0xFF) == rx->buf[rx->bufpos - 1]))
-               {
-                  crcvalid = 1;
-               }
-
-               if (crcvalid)
-               {
-                 PROT_Packet(&rx->buf[0], rx->bufpos - 2);
-               }
-               else
-               {
-                   PL_Printf(DBG_DEBUG, "invalid CRC\n");
-               }
+                rx->bufpos = 0; /* invalid */
             }
+            else
+            {
+                if (rx->bufpos > 2)
+                {
+                    for (crc = 0, i = 0; i < rx->bufpos - 2; i++)
+                        crc += rx->buf[i];
+
+                    crc = (~crc + 1);
+                    crc1 = (rx->buf[rx->bufpos - 1] << 8) + rx->buf[rx->bufpos - 2];
+
+                    if (crc1 == crc)
+                    {
+                        PROT_Packet(&rx->buf[0], rx->bufpos - 2);
+                    }
+                    else
+                    {
+                        err += 1;
+                    }
+                }
+                rx->bufpos = 0;
+            }
+            rx->escaped &= ~ASC_FLAG;
+            goto nextTurn;
+        }
+        else if (c == FR_ESC)
+        {
+            rx->escaped |= ASC_FLAG;
+            goto nextTurn;
+        }
+
+        if (rx->escaped & ASC_FLAG)
+        {
+            /* translate the 2-byte escape sequence back to original char */
+            rx->escaped &= ~ASC_FLAG;
+
+            switch (c)
+            {
+                case T_FR_ESC: c = FR_ESC; break;
+                case T_FR_END: c = FR_END; break;
+                default:
+                    rx->bufpos = 0; /* invalid */
+            }
+        }
+
+        /* we reach here with every byte for the buffer. */
+        if (rx->bufpos < sizeof(rx->buf))  /* Prevent buffer overflow by reserving space for CRC */
+        {
+            rx->buf[rx->bufpos++] = c;
+        }
+        else
+        {
+            /* Buffer is full, discard the byte and reset state */
             rx->bufpos = 0;
-            rx->crc = 0;
-         }
-         rx->escaped &= ~ASC_FLAG;
-         goto nextTurn;
+        }
+    }
 
-      case FR_ESC:
-         rx->escaped |= ASC_FLAG;
-         goto nextTurn;
-      }
-
-      if (rx->escaped & ASC_FLAG)
-      {
-         /* translate the 2 byte escape sequence back to original char */
-         rx->escaped &= ~ASC_FLAG;
-
-         switch (c)
-         {
-         case T_FR_ESC: c = FR_ESC; break;
-         case T_FR_END: c = FR_END; break;
-         default: goto nextTurn;
-         }
-      }
-
-      /* we reach here with every byte for the buffer
-         legacy BUG: checksum bytes are added but should not be */
-      if (rx->bufpos < sizeof(rx->buf))
-      {
-         rx->buf[rx->bufpos++] = c;
-         rx->crc += c;
-      }
-   }
+    return err;
 }
--- protocol.h
+++ protocol.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 dresden elektronik ingenieurtechnik gmbh.
+ * Copyright (c) 2021-2026 dresden elektronik ingenieurtechnik gmbh.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -13,14 +13,14 @@
 
 typedef struct {
     unsigned bufpos;
-    unsigned short crc;
     unsigned char escaped;
     unsigned char buf[256];
 } PROT_RxState;
 
 /* Platform independent declarations. */
 void PROT_SendFlagged(const unsigned char *data, unsigned len);
-void PROT_ReceiveFlagged(PROT_RxState *rx, const unsigned char *data, unsigned len);
+/* Returns >0 on CRC errors. */
+int PROT_ReceiveFlagged(PROT_RxState *rx, const unsigned char *data, unsigned len);
 void PROT_Packet(const unsigned char *data, unsigned len);
 
 /*! Platform specific declarations.
